a808c08ac4a8f6ac14755aa77edad398d33c4cab
yanoof <yanoof.03@gmail.com>
2025-08-11 23:22:24 +0500

    edited db migrations

--- database/migrations/2025_08_06_173749_create_boats_table.php
+++ database/migrations/2025_08_06_173749_create_boats_table.php
@@ -14,21 +14,14 @@ return new class extends Migration
             Schema::create('boats', function (Blueprint $table) {
                 $table->id();
                 $table->string('boat_id')->unique();
-
                 $table->string('name')->nullable();
                 $table->decimal('latitude', 10, 7)->nullable();
                 $table->decimal('longitude', 10, 7)->nullable();
                 $table->float('speed')->nullable();
                 $table->unsignedSmallInteger('heading')->nullable();
-
-                // If API timestamp might be missing or not ISO, make it nullable
                 $table->timestamp('datetime')->nullable();
-
                 $table->string('contact')->nullable();
-
-                // Not JSON for SQLite; store as TEXT (comma-joined or JSON string)
                 $table->text('atolls')->nullable();
-
                 $table->string('type')->nullable();
             });
         }
