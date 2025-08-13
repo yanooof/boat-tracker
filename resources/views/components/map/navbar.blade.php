79bea21e0dd3625a90fa5e99d77379abea0a4920
naavi.ahmed <s2101848@students.villacollege.edu.mv>
2025-08-13 16:33:57 +0500

    Added sidebar button to navbar.

--- resources/views/components/map/navbar.blade.php
+++ resources/views/components/map/navbar.blade.php
@@ -1,4 +1,14 @@
 <div class="navigation-bar">
+
+    <button id="sidebarBtn", class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-1 px-1 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+    <svg width="30px" height="30px" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="48" height="48" fill="white" fill-opacity="0.01"/>
+    <path d="M7.94977 11.9498H39.9498" stroke="#FFFFFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M7.94977 23.9498H39.9498" stroke="#FFFFFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M7.94977 35.9498H39.9498" stroke="#FFFFFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    </button>
+
     <div class="logo">BoatTracker</div>
 
     <div class="search-wrap">
